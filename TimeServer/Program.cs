f675ddf1190a6975087771f21ff38344c735cdf6
Cecilija-Simic-Rihtnesberg <cecilija.simic.rihtnesberg@student.forsbergsskola.se>
2022-09-04 12:58:44 +0200

    Add Time Server

---
+++ TimeServer/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace TimeServerCSR
+{
+    class Program
+    
+    {
+        private static void Main(string[] args) 
+        
+        {
+            TcpListener tcpListener = new TcpListener(IPAddress.Any, 44444);
+            tcpListener.Start();
+            
+            Console.WriteLine("Connecting!");
+            
+            while (true) 
+            
+            {
+                var tcpClient = tcpListener.AcceptTcpClient();
+                
+                //Console.WriteLine("Waiting for connection...");
+                Console.WriteLine($"Client {tcpClient.Client.RemoteEndPoint} connected.");
+                
+                using var stream = tcpClient.GetStream();
+            
+                stream.Write(Encoding.ASCII.GetBytes($"Hi, this is Cecilijas TimeServer. The current Date & Time is: " + DateTime.Now));
+                
+                tcpClient.Close();
+            }
+        }
+    }
+}
