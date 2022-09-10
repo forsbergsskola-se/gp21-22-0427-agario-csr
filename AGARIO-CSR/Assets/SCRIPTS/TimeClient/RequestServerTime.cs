b661cdfce494fd31ff88bd297852b934acdafda1
Cecilija-Simic-Rihtnesberg <cecilija.simic.rihtnesberg@student.forsbergsskola.se>
2022-09-10 15:13:06 +0200

    Update RequestServerTime.cs

--- AGARIO-CSR/Assets/SCRIPTS/TimeClient/RequestServerTime.cs
+++ AGARIO-CSR/Assets/SCRIPTS/TimeClient/RequestServerTime.cs
@@ -4,33 +4,44 @@ using System.Text;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class RequestServerTime : MonoBehaviour
-
-{
-    public Text Text_DateAndTimeNowCSR;
-
-    public void SendRequest() 
-    
-    {
-        TcpClient tcpClient = new TcpClient("127.0.0.1", 44444);
+public class RequestServerTime : MonoBehaviour {
+  [SerializeField] Text text_DateAndTimeNowCSR ;// text_DateAndTimeNowCSR [SerializeField] 
+  //private int numBer;
+ 
+   public void SendRequest()
+   {
+        int port = 444444;
+        TcpClient tcpClient = new TcpClient("127.0.0.1", port);
 
         var serverStream = tcpClient.GetStream();
-        Console.WriteLine(serverStream);
-        
+        //Console.WriteLine(serverStream);
+
         byte[] buffer = new byte[100];
-        
+
         serverStream.Read(buffer, 0, 100);
-        
+
         string dateTime = Encoding.ASCII.GetString(buffer);
 
-        ChangeText(dateTime);
-        Console.WriteLine(dateTime);
-    }
+       ChangeText(dateTime);
+      // Console.WriteLine(dateTime);
+   }
     
-
-    public void ChangeText(string dateTime) 
+    // }
+   // Console.WriteLine("dateTime");
     
+   // void Start()
+   // { 
+   //     textField = GameObject.Find("Text_DateAndTimeNowCSR").GetComponent<Text>();
+   // }
+    
+    public void ChangeText(string dateTime) //string dateTime
     {
-        Text_DateAndTimeNowCSR.text = dateTime;
+     text_DateAndTimeNowCSR.text = "Date and Time now:" + dateTime;
+                                              // dateTime = numBer;
+                                              // numBer += 10;
+                                              // Console.WriteLine(numBer);
+                                              /*_number += 10;
+                                                textField.text = "Date and Time now:" + _number;*/
+                                                //Text_DateAndTimeNowCSR.text = dateTime; "Date and Time now:" + 
     }
 }
