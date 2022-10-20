5a11794bc1d43ad4f19d0741311cb9551d3d89aa
Cecilija-Simic-Rihtnesberg <cecilija.simic.rihtnesberg@student.forsbergsskola.se>
2022-10-20 11:52:26 +0200

    Update Program.cs

--- AGARIO-CSR/TimeServer/Program.cs
+++ AGARIO-CSR/TimeServer/Program.cs
@@ -1,33 +1,23 @@
-﻿using System;
-using System.Net;
+﻿using System.Net;
 using System.Net.Sockets;
 using System.Text;
 
-namespace TimeServerCSR
+TcpListener tcpListener = new TcpListener(IPAddress.Any, 44444);
+tcpListener.Start();
+
+Console.WriteLine("Connecting!");
+            
+while (true) 
+            
 {
-    class Program {
-        private static void Main(string[] args) 
-        
-        {
-            TcpListener tcpListener = new TcpListener(IPAddress.Any, 44444);
-            tcpListener.Start();
-            //Console.WriteLine(tcpListener);
-            Console.WriteLine("Connecting!");
-            
-            while (true) 
-            
-            {
-                var tcpClient = tcpListener.AcceptTcpClient();
-                Console.WriteLine(tcpClient);
-                //Console.WriteLine("Waiting for connection...");
-                Console.WriteLine($"Client {tcpClient.Client.RemoteEndPoint} connected.");
+    var tcpClient = tcpListener.AcceptTcpClient();
+    Console.WriteLine(tcpClient);
+    
+    Console.WriteLine($"Client {tcpClient.Client.RemoteEndPoint} connected.");
                 
-                using var stream = tcpClient.GetStream();
+    using var stream = tcpClient.GetStream();
             
-                stream.Write(Encoding.ASCII.GetBytes($"Hi, this is Cecilijas TimeServer. The current Date & Time is: " + DateTime.Now));
+    stream.Write(Encoding.ASCII.GetBytes($"Hi, this is Cecilijas TimeServer. The current Date & Time is: " + DateTime.Now));
                 
-                tcpClient.Close();
-            }
-        }
-    }
+    tcpClient.Close();
 }
