8d52d328ddc246b56819d6789f0b3107bf3256ce
Cecilija-Simic-Rihtnesberg <cecilija.simic.rihtnesberg@student.forsbergsskola.se>
2022-10-20 19:13:28 +0200

    Create OpenWordServer

---
+++ AGARIO-CSR/WordServer/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+public class OpenWordServer
+{
+    private const int maxLenght = 20;
+    private static string message;
+
+    private static IPEndPoint remoteEP = new(IPAddress.Loopback, 44444);
+    private static UdpClient _udpClient = new(remoteEP);
+
+    private static void Main()
+    {
+        while (true)
+        {
+            IPEndPoint remoteEndopint = default;
+            var data = _udpClient.Receive(ref remoteEndopint);
+            var massageString = Encoding.ASCII.GetString(data).Trim();
+
+            if (IsValid(massageString)) message += " " + massageString;
+
+            _udpClient.Send(Encoding.ASCII.GetBytes(message), message.Length, remoteEndopint);
+        }
+    }
+
+    private static bool IsValid(String text) => text.Length > 0 && text.Length <= maxLenght;
+}
